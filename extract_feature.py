f9ef1934f03df527727c76c5b0ee142b7a38fc9c
Ryan Rafe Lee <holo@wisewolf.org>
2017-02-23 20:33:55 +0900

    Added train_batch. Fixed some bugs

--- extract_feature.py
+++ extract_feature.py
@@ -5,10 +5,10 @@ import fft
 import math
 from oct2py import octave
 
-def get_sequence(name):
+def get_sequence(name, feature_number):
 	with open(name) as f:
 		seqs= f.read().upper().split('>')
-		seqs= list(filter(lambda seq: len(seq)>15000, map(lambda seq: ''.join(seq.split('\n')[1:]),seqs)))
+		seqs= list(filter(lambda seq: len(seq)>feature_number*2, map(lambda seq: ''.join(seq.split('\n')[1:]),seqs)))
 	return seqs
 
 def calc_z_curve(sequence):
@@ -44,9 +44,9 @@ def extract_features(fz, features= 1000):
 	yf=[]
 	zf=[]
 	for i in range(features):
-		xf.append(max(fz[0][int(length/features*i):int(length/features*(i+1))]))
-		yf.append(max(fz[1][int(length/features*i):int(length/features*(i+1))]))
-		zf.append(max(fz[2][int(length/features*i):int(length/features*(i+1))]))
+		xf.append(max(np.concatenate((fz[0][int(length/features*i):max(int(length/features*(i+1)),int(length/features*i)+1)],[0]))))
+		yf.append(max(np.concatenate((fz[1][int(length/features*i):max(int(length/features*(i+1)),int(length/features*i)+1)],[0]))))
+		zf.append(max(np.concatenate((fz[2][int(length/features*i):max(int(length/features*(i+1)),int(length/features*i)+1)],[0]))))
 	
 	xf= np.divide(xf, np.max(xf))
 	yf= np.divide(yf, np.max(yf))
@@ -56,9 +56,10 @@ def extract_features(fz, features= 1000):
 if __name__=='__main__':
 	taxonomy= sys.argv[1]
 	file_list= list(filter(lambda x: 'fna' == x[-3:], os.listdir(taxonomy)))
+	feature_number= 2000
 	for seq_file in file_list:
 		print(seq_file)
-		seqs= get_sequence(taxonomy+'/'+seq_file)
+		seqs= get_sequence(taxonomy+'/'+seq_file, feature_number)
 		if len(seqs)==0:
 			continue
 		feature_idx=1
@@ -66,6 +67,6 @@ if __name__=='__main__':
 			print(seq_file+"_"+str(feature_idx))
 			z_curve= calc_z_curve(seq)
 			fft_result= z_curve_fft(z_curve)
-			feature= extract_features(fft_result)
+			feature= extract_features(fft_result, feature_number)
 			np.save(taxonomy+'/'+ seq_file+'_'+str(feature_idx), feature)
 			feature_idx+=1
--- train_batch.py
+++ train_batch.py
@@ -7,13 +7,22 @@ def load_data(taxonomy):
 	data_list= list(filter(lambda x:x[-3:]=='npy', os.listdir(taxonomy)))
 	result=[]
 	for data_file in data_list:
-		result.append(np.load('%s/%s' %(taxonomy, data_file)))
+		result.append(np.load('%s/%s' %(taxonomy, data_file))[0:1000])
 		if len(result)%1000== 0:
 			print("%s: %d/%d" %(taxonomy, len(result), len(data_list)))
 
 	return np.array(result, dtype='f')
 
-def train(steps=10000):
+def get_batch(data, batch_size, label):
+	np.random.shuffle(data)
+	x_train= data[0:min(batch_size, len(data))]
+	y_train=[]
+	for i in range(batch_size):
+		y_train.append([label])
+	y_train= np.array(y_train)
+	return (x_train, y_train)
+
+def train(batch_size=1000, steps_per_batch=1000, whole_step= 1000):
 	archaea_data= load_data('archaea')
 	bacteria_data= load_data('bacteria')
 	protozoa_data= load_data('protozoa')
@@ -33,20 +42,8 @@ def train(steps=10000):
 	protozoa_val= protozoa_data[int(len(protozoa_data)-val_len):]
 	fungi_val= fungi_data[int(len(fungi_data)-val_len):]
 
-	x_train= np.concatenate((archaea_train, bacteria_train, protozoa_train, fungi_train), axis=0)
-	y_train= []
 	x_val= np.concatenate((archaea_val, bacteria_val, protozoa_val, fungi_val), axis=0)
 	y_val= []
-	for i in range(len(archaea_train)):
-		y_train.append([0])
-	for i in range(len(bacteria_train)):
-		y_train.append([1])
-	for i in range(len(protozoa_train)):
-		y_train.append([2])
-	for i in range(len(fungi_train)):
-		y_train.append([3])
-	y_train= np.array(y_train)
-
 	for i in range(len(archaea_val)):
 		y_val.append([0])
 	for i in range(len(bacteria_val)):
@@ -77,10 +74,18 @@ def train(steps=10000):
 	import logging
 	logging.getLogger().setLevel(logging.INFO)
 
-	feature_columns = [tf.contrib.layers.real_valued_column("", dimension=x_train.shape[1])]
+	feature_columns = [tf.contrib.layers.real_valued_column("", dimension=x_val.shape[1])]
 
-	clf= learn.DNNClassifier(feature_columns=feature_columns, hidden_units=[int(x_train.shape[1]/10),int(x_train.shape[1]/50)], n_classes=4, model_dir='./dnn_model')
-	clf.fit(x=x_train, y=y_train, steps=steps)
+	clf= learn.DNNClassifier(feature_columns=feature_columns, hidden_units=[int(x_val.shape[1]/5),int(x_val.shape[1]/50)], n_classes=4, model_dir='./batch_dnn_model')
+
+	for i in range(whole_step):
+		(x_t0, y_t0)= get_batch(archaea_train, batch_size, 0)
+		(x_t1, y_t1)= get_batch(bacteria_train, batch_size, 1)
+		(x_t2, y_t2)= get_batch(protozoa_train, batch_size, 2)
+		(x_t3, y_t3)= get_batch(fungi_train, batch_size, 3)
+		x_train= np.concatenate((x_t0, x_t1, x_t2, x_t3))
+		y_train= np.concatenate((y_t0, y_t1, y_t2, y_t3))
+		clf.fit(x=x_train, y=y_train, steps=steps_per_batch)
 
 	predictions= np.array(list(clf.predict(np.asarray(x_val, dtype=np.float32))))
 	predictions.shape= y_val.shape
